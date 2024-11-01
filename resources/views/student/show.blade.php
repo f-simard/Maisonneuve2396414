f03d460e8456b49d82ef962fa579d7f0cf0484a0
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-11-01 14:12:42 -0400

    modify style adaptation

--- resources/views/student/show.blade.php
+++ resources/views/student/show.blade.php
@@ -1,36 +1,36 @@
 @extends('layouts.app')
 @section('title', 'Student List')
 @section('content')
-<div class="d-flex align-items-center gap-4">
+<div class="d-sm-flex align-items-center gap-4">
 	<h1>{{$student->name}}</h1>
 	<a href="{{ route('student.edit', $student->id) }}" class="btn btn-warning btn-sm">Edit</a>
 </div>
 <div class="container-md col-12 col-lg-6 mx-auto ms-lg-4 mt-4">
-	<div class="container-md d-flex align-items-center justify-content-between flex-wrap bg-light bg-gradient">
+	<div class="container-lg d-sm-flex align-items-center justify-content-between mt-2 bg-light bg-gradient">
 		<p class="p-1 m-0">ID</p>
 		<p class="p-1 m-0">{{$student->id}}</p>
 	</div>
-	<div class="container-lg d-flex justify-content-between flex-wrap mt-2">
+	<div class="container-lg d-sm-flex flex-md-row align-items-center justify-content-between mt-2">
 		<p class="p-1 m-0">Name</p>
 		<p class="p-1 m-0">{{$student->name}}</p>
 	</div>
-	<div class="container-md d-flex align-items-center justify-content-between flex-wrap bg-light bg-gradient">
+	<div class="container-lg d-sm-flex flex-md-row align-items-center justify-content-between mt-2 bg-light bg-gradient">
 		<p class="p-1 m-0">Address</p>
 		<p class="p-1 m-0">{{$student->address}}</p>
 	</div>
-	<div class="container-md d-flex justify-content-between flex-wrap mt-2">
+	<div class="container-lg d-sm-flex align-items-center justify-content-between mt-2">
 		<p class="p-1 m-0">City</p>
 		<p class="p-1 m-0">{{$student->city->name}}</p>
 	</div>
-	<div class="container-md d-flex align-items-center justify-content-between flex-wrap bg-light bg-gradient">
+	<div class="container-lg d-sm-flex flex-md-row align-items-center justify-content-between mt-2 bg-light bg-gradient">
 		<p class="p-1 m-0">Phone number</p>
 		<p class="p-1 m-0">{{$student->phone}}</p>
 	</div>
-	<div class="container-md d-flex justify-content-between flex-wrap mt-2">
+	<div class="container-lg d-sm-flex flex-md-row align-items-center justify-content-between mt-2">
 		<p class="p-1 m-0">Email</p>
 		<p class="p-1 m-0">{{$student->email}}</p>
 	</div>
-	<div class="container-md d-flex align-items-center justify-content-between flex-wrap bg-light bg-gradient">
+	<div class="container-lg d-sm-flex flex-md-row align-items-center justify-content-between mt-2 bg-light bg-gradient">
 		<p class="p-1 m-0">Birthday</p>
 		<p class="p-1 m-0">{{$student->birthday}}</p>
 	</div>
