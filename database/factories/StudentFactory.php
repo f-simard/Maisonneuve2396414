a0656f6b03f462a7feee87d7bbe4b6a76d2f7482
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-11-23 09:56:32 -0500

    modify student factory based on feedback

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\City;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class StudentFactory extends Factory
@@ -19,7 +20,7 @@ class StudentFactory extends Factory
 			'phone' => $this->faker->regexify('\d{3}-\d{3}-\d{4}'),
 			'email' => $this->faker->unique()->safeEmail(),
 			'birthday' => $this->faker->dateTimeBetween('-50 year', '-16 year')->format('Y-m-d'),
-			'city_id' => $this->faker->numberBetween(1, 15)
+			'city_id' => City::all()->random()->id
 		];
 	}
 }
